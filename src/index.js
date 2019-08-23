a0ff4e14a3489e2b4266634ded061a0e882cd2b7
LareinaB <1354636619@qq.com>
2019-08-23 23:22:21 +0800

    初步配置，多入口chunk的关系

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
+// 加载全局变量 会先将该模块加载出来 然后将模块导出的对象挂载到window下面
+// 在其他页面使用的时候window.$就可以
+let $ = require('expose-loader?$!jquery');
+
 console.log('hello');
+document.getElementById('app').innerHTML = 'javascript';
+
+// CSS不是JS模块所以需要转换 loader工具
+require('./index.css');
---
+++ webpack.config.js
@@ -0,0 +1,72 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+    // 单入口 字符串/数组 ['./src/index.js', './src/base.js']
+    // 多入口 对象 先找到每个入口 从各个入口分别出发找到依赖的模块
+    // 生成一个代码块chunk，最后把代码块写到文件系统中(Assets)
+    entry: {
+        index: './src/index.js',
+        base: './src/base.js',
+        common: './src/common.js'
+    },
+    // 输出 经过一系列处理得出最终想要的代码结果
+    output: {
+        path: path.join(__dirname, 'dist'),
+        // name是entry的名字main hash根据打包后的文件计算出来的一个hash值
+        // [name].[hash].js
+        filename: "[name].[hash].js"
+    },
+    // 模块转换器
+    module: {
+        rules: [
+            {
+                test: /\.css$/, // 转换文件的匹配正则
+                // css-loader用来解析CSS文件中的url路径
+                // style-loader 可以把CSS文件编程style标签插入HEAD中
+                // 多个loader是从右往左处理的
+                loader: ["style-loader", "css-loader"],
+            }
+        ]
+    },
+    // 扩展插件
+    plugins: [
+        new CleanWebpackPlugin({
+            cleanOnceBeforeBuildPatterns: [path.join(__dirname, 'dist')]
+        }),
+        // 此插件可以自动产出html文件 如果是多个并且生成多个页面分别引用
+        // 这里要用chunks对应起来 一个entry对应一个chunk
+        new HtmlWebpackPlugin({
+            // 指定模板
+            template: './src/1.index.html',
+            // 指定产出
+            filename: 'index.html',
+            chunks: ['index', 'common'], // 在产出的html文件中加那些代码块
+            title: 'hello webpack',
+            hash: true, // 会在引入的js里加入查询字符串避免缓存
+            minify: { // 会在引用时去掉双引号
+                removeAttributeQuotes: true
+            }
+        }),
+        new HtmlWebpackPlugin({
+            // 指定模板
+            template: './src/1.index.html',
+            // 指定产出
+            filename: 'base.html',
+            title: 'hello base',
+            chunks: ['base', 'common'],
+            hash: true, // 会在引入的js里加入查询字符串避免缓存
+            minify: { // 会在引用时去掉双引号
+                removeAttributeQuotes: true
+            }
+        })
+
+    ],
+    // 配置此静态文件服务器 可以用来预览打包后的项目
+    devServer: {
+        contentBase: './dist',
+        host: 'localhost',
+        port: 8080,
+        compress: true, // 服务器返回给浏览器的时候是否启动gzip压缩
+    }
+};
